371bca549fe9f96795247d239c6fd4f96e2cf040
Akhmed Adzhavi <alex_adzhavi@yahoo.com>
2023-02-13 17:44:16 -0500

    Munged and analyzed

--- analyze.py
+++ analyze.py
@@ -1,1 +1,24 @@
-# Place code below to do the analysis part of the assignment.
+import csv
+
+h = open("data/clean_data.csv")
+
+reader = csv.reader(h)
+header = next(reader)
+
+count = 1
+sum = 0.0
+
+for row in reader:
+    if (count == 1):
+        start = row[0] # Store the year of the first year in a decade
+        sum = float(row[13]) # Store the annual mean J-D
+    if (count == 3 and row[0] == "2022"): # Fixes that we don't have data after 2022
+        print(start + " to " + row[0] + " -> {:.2f}".format(sum / count)) 
+    if (count == 10): # When we reach the 10th year, print out
+        print(start + " to " + row[0] + " -> {:.2f}".format(sum / count)) 
+        count = 1 # Reset the count
+    else:
+        sum = sum + float(row[13]) 
+        count = count + 1
+
+h.close()
--- munge.py
+++ munge.py
@@ -1,1 +1,36 @@
-# Place code below to do the munging part of this assignment.
+f = open('data/raw_data.txt', 'r')
+g = open('data/clean_data.csv', 'w')
+
+count = 0
+
+for x in f:
+    if (count <= 6 or count >= 165): # Skip over the unnecessary rows
+        count = count + 1
+        continue
+    x = x.strip()
+    l = x.split()
+    if (len(l) == 0): # If it is an empty row, then skip it
+        count = count + 1
+        continue
+    if (l[0] == "Year" and count > 7): # Skip over the headings after the 8th row
+        count = count + 1
+        continue
+    for i in range(len(l)-1):
+        if (l[i] == "***"):
+            l[i] = l[i-1] # We are missing the annual mean D-N in 1880, we will just estimate this with J-D in the same year
+        if (l[i] == "****"):
+            l[i] = str((int(l[1]) + int(l[2]))//2) # We are missing the DJF value, estimate by mean of J and F
+        if (count == 7 or i == 0): # If the row is just the headers or the element is the year, then just write it
+            g.write(l[i])
+        else: # Otherwise, convert it into degrees F
+            temp = (int(l[i])*1.8)/100
+            g.write("{:.1f}".format(temp))
+        if (i == len(l)-2): # Skip over the last column over years
+            break
+        else:
+            g.write(",")
+    count = count + 1
+    g.write("\n")  
+
+f.close()
+g.close()  
